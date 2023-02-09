dfa55499273ece93ca9d128fea0ed2d59aa95a52
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2023-02-09 19:06:31 +0100

    Update setup-ros2.sh

--- setup-ros2.sh
+++ setup-ros2.sh
@@ -4,6 +4,9 @@ cd ~/ros2_ws/src
 git clone https://gitlab.com/eufs/eufs_sim.git
 git clone https://gitlab.com/eufs/eufs_msgs.git
 git clone https://gitlab.com/eufs/eufs_rviz_plugins.git
+git clone https://github.com/arece-fs/arece_lidar_cpp.git
+git clone https://github.com/arece-fs/arece_msgs.git
+git clone https://github.com/arece-fs/arece_marker_debug_py
 sudo apt-get install python3-pip -y
 pip3 install colcon-common-extensions -U
 export EUFS_MASTER=~/ros2_ws/src
@@ -22,4 +25,5 @@ sudo apt-get install ros-galactic-ackermann-msgs -y
 sudo apt-get install ros-galactic-joint-state-publisher -y
 sudo apt-get install ros-galactic-xacro -y
 sudo apt-get install gazebo11 libgazebo11 libgazebo11-dev -y
+sudo apt-get install libpcl-dev -y
 colcon build
