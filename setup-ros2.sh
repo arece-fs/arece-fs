8ae8269d04b286a99b740531af45316219a0f3d7
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2023-04-29 21:30:07 +0200

    switch form eufs to arece-sim

--- setup-ros2.sh
+++ setup-ros2.sh
@@ -1,20 +1,19 @@
 sudo apt-get install git -y
 mkdir -p ~/ros2_ws/src
 cd ~/ros2_ws/src
-git clone https://gitlab.com/eufs/eufs_sim.git
 git clone https://gitlab.com/eufs/eufs_msgs.git
 git clone https://gitlab.com/eufs/eufs_rviz_plugins.git
-git clone https://github.com/arece-fs/arece_lidar_cpp.git
+git clone https://github.com/arece-fs/arece-sim.git
 git clone https://github.com/arece-fs/arece_msgs.git
 git clone https://github.com/arece-fs/arece_marker_debug_py
 sudo apt-get install python3-pip -y
 pip3 install colcon-common-extensions -U
-export EUFS_MASTER=~/ros2_ws/src
-echo 'export EUFS_MASTER=~/ros2_ws/src' >> ~/.bashrc
+export ARECE_MASTER=~/ros2_ws/src
+echo 'export ARECE_MASTER=~/ros2_ws/src' >> ~/.bashrc
 sudo apt-get install python3-rosdep -y
 sudo rosdep init
 rosdep update
-rosdep install --from-paths $EUFS_MASTER --ignore-src -r -y
+rosdep install --from-paths $ARECE_MASTER --ignore-src -r -y
 cd ~/ros2_ws
 echo 'source ~/ros2_ws/install/setup.bash' >> ~/.bashrc
 source ~/ros2_ws/install/setup.bash
@@ -26,7 +25,7 @@ sudo apt-get install ros-galactic-ackermann-msgs -y
 sudo apt-get install ros-galactic-joint-state-publisher -y
 sudo apt-get install ros-galactic-xacro -y
 sudo apt-get install gazebo11 libgazebo11 libgazebo11-dev -y
-sudo apt-get install libpcl-dev -y
+sudo apt-get install libpcl-dev libyaml-cpp-dev -y
 sudo apt-get install ros-galactic-plotjuggler -y
 source ~/.bashrc
 colcon build
