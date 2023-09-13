c711beafcd6795527a2066b4596f0ac9ce32e545
Pierre SIMOND <70819571+swiipius@users.noreply.github.com>
2023-09-13 11:01:57 -0400

    Clean other files

--- arece_doc.sh
+++
@@ -1,7 +0,0 @@
-rosdoc2 build --package-path $1
-rm -rf cross_reference/ docs_build/
-mkdir docs
-IFS='/' read -ra ADDR <<< $(pwd)
-package="${ADDR[-1]}";
-mv docs_output/$package/* docs/
-rm -rf docs_output/
