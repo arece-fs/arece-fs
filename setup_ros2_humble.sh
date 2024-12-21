254399afb50fa6445f8b87d27b3ad3d4be26f839
Raphaël <raphael.jeantet@arece.eu>
2024-12-21 16:33:31 +0100

    update repos and dep from requirement

--- setup_ros2_humble.sh
+++ setup_ros2_humble.sh
@@ -1,24 +1,59 @@
 sudo apt-get install git -y
 cd ~/
-wget https://github.com/cli/cli/releases/download/v2.31.0/gh_2.31.0_linux_amd64.deb
-sudo dpkg -i gh_2.31.0_linux_amd64.deb -y
-rm gh_2.31.0_linux_amd64.deb
-gh auth login
 mkdir -p ~/ros2_ws/src
 cd ~/ros2_ws/src
+
+sudo apt-get install python3-rosdep python3-rosinstall-generator python3-vcstool python3-rosinstall build-essential
+
+# install zed_sdk 
+cd ~
+mkdir -p ~/zed_ws/src
+cd ~/zed_ws/src
+git clone  --recursive https://github.com/stereolabs/zed-ros2-wrapper.git
+cd ..
+rm -r build # in case of multiple script run, can create linking problems
+sudo apt update
+rosdep install --from-paths src --ignore-src -r -y # install dependencies
+colcon build --symlink-install --cmake-args=-DCMAKE_BUILD_TYPE=Release --parallel-workers $(nproc) # build the workspace
+echo source $(pwd)/install/local_setup.bash >> ~/.bashrc # automatically source the installation in every new bash (optional)
+export ARECE_MASTER=~/ros2_ws/src
+echo 'export ARECE_MASTER=~/ros2_ws/src' >> ~/.bashrc
+source ~/.bashrc
+
+cd $ARECE_MASTER
+# git arece repos 
 git clone https://github.com/arece-fs/arece-sim.git
 git clone https://github.com/arece-fs/arece_msgs.git
 git clone https://github.com/arece-fs/eufs_msgs.git
+git clone https://github.com/arece-fs/arece_compet_python.git
+git clone https://github.com/arece-fs/arece_state_machine_python.git 
+git clone https://github.com/arece-fs/lap_detection.git 
+git clone https://github.com/arece-fs/arece_lidar_cpp.git
+git clone --recursive https://github.com/arece-fs/FAST_LIO_ROS2.git
+git clone https://github.com/arece-fs/arece_perception_cpp.git
+git clone https://github.com/arece-fs/arece_stereo_python.git
+git clone --recursive https://github.com/arece-fs/ros_can.git
+git clone https://github.com/arece-fs/arece_trajectory_planner_python.git
+git clone https://github.com/arece-fs/arece_gps.git
+git clone https://github.com/arece-fs/arece_marker_debug_cpp.git
+
+# set branchs 
 cd ~/ros2_ws/src/arece-sim
 git checkout humble
+cd ~/ros2_ws/src/
+
+# install deps
+cd arece_trajectory_planner_python
+pip install -r requirements.txt
+cd ..
 cd ~/ros2_ws/src
 sudo apt-get install python3-pip -y
 pip3 install colcon-common-extensions -U
-export ARECE_MASTER=~/ros2_ws/src
-echo 'export ARECE_MASTER=~/ros2_ws/src' >> ~/.bashrc
+
 sudo apt-get install python3-rosdep -y
 sudo rosdep init
 rosdep update
+
 rosdep install --from-paths $ARECE_MASTER --ignore-src -r -y
 cd ~/ros2_ws
 echo 'source ~/ros2_ws/install/setup.bash' >> ~/.bashrc
